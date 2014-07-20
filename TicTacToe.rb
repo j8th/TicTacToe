0ea3be41d076f6d1eb232fa9a1b546ab36cd1f9c
Archer <jkmail308@gmail.com>
2014-07-20 18:05:15 +0900

    Moved the App class into its own file.

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -4,6 +4,7 @@ require_relative 'lib/game'
 require_relative 'lib/human'
 require_relative 'lib/ai'
 require_relative 'lib/board'
+require_relative 'lib/app'
 
 require 'optparse'
 
@@ -21,42 +22,6 @@ end.parse!
 $DEBUG = options[:debug]
 
 
-class App
-	def initialize
-		puts "Welcome to Tic Tac Toe!"
-		puts "Who is human, X or O?  "
-		choice = STDIN.gets.chomp.downcase
-		case choice
-		when 'x'
-			@human_is_X = true
-		when 'o'
-			@human_is_X = false
-		else
-			puts "We'll just make you X..."
-			@human_is_X = true
-		end
-	end
-
-	def run
-		puts ""
-		
-		playerX = @human_is_X ? Human.new('Fry') : AI.new('Bender')
-		playerO = @human_is_X ? AI.new('Bender') : Human.new('Fry')
-
-		@game = Game.new(
-			playerX,
-			playerO,
-			Board.new
-		)
-		@game.run
-
-		puts "Would you like to play again?  (y/n)"
-		choice = STDIN.gets.chomp
-		if choice.downcase == 'y'
-			run
-		end
-	end
-end
 
 app = App.new( )
 app.run
