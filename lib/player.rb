f4c44a10b818b0f4face07c412ab3ef8a66edd0f
Apprentice <apprentice@Apprentices-MacBook-Pro.local>
2014-10-24 09:58:38 -0500

    Theoretically got rid of all the tabs for 4 spaces.

--- lib/player.rb
+++ lib/player.rb
@@ -1,30 +1,30 @@
 
 
 class Player
-	attr_reader :name
+    attr_reader :name
 
-	def initialize( name )
-		raise ArgumentError, "name must be a string." unless name.is_a? String
+    def initialize( name )
+        raise ArgumentError, "name must be a string." unless name.is_a? String
 
-		@name = name
-	end
-
-
-
-	# Public Methods
-
-	def draw
-		UI.msg("Name: #{@name}")
-	end
+        @name = name
+    end
 
 
 
-	# "Interface" Methods
+    # Public Methods
 
-	# The player makes a move on the board.
-	# This method drops a piece on the board (the board object is actually changed).
-	def move( board )
-		raise NotImplementedError, "Implement this method in a child class."
-	end
+    def draw
+        UI.msg("Name: #{@name}")
+    end
+
+
+
+    # "Interface" Methods
+
+    # The player makes a move on the board.
+    # This method drops a piece on the board (the board object is actually changed).
+    def move( board )
+        raise NotImplementedError, "Implement this method in a child class."
+    end
 
 end
--- lib/ui.rb
+++ lib/ui.rb
@@ -1,18 +1,18 @@
 class UI
-	# Display a text message to the user.
-	def self.msg( string )
-		puts string
-	end
+    # Display a text message to the user.
+    def self.msg( string )
+        puts string
+    end
 
-	# Display a text message to the user
-	def self.prompt( instruction_message, choices )
-		puts instruction_message
-		input = STDIN.gets.chomp
-		return input if choices.include? input
-		return input.to_i if choices.include? input.to_i
-		return input.downcase if choices.include? input.downcase
-		return input.upcase if choices.include? input.upcase
-		puts 'That is not a valid option.'
-		return prompt(instruction_message, choices)
-	end
+    # Display a text message to the user
+    def self.prompt( instruction_message, choices )
+        puts instruction_message
+        input = STDIN.gets.chomp
+        return input if choices.include? input
+        return input.to_i if choices.include? input.to_i
+        return input.downcase if choices.include? input.downcase
+        return input.upcase if choices.include? input.upcase
+        puts 'That is not a valid option.'
+        return prompt(instruction_message, choices)
+    end
 end
